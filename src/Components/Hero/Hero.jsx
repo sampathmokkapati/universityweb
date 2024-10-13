006ef252eb80c20c5c5d74659e247ae890eb3ac8
sampathmokkapati <sampath@sampaths-MacBook-Air.local>
2024-10-13 22:29:29 +0530

    i

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Hero.css'
-import dark_arrow from '../../assets/dark_arrow.png'
+import dark_arrow from '../../assets/dark-arrow.png'
 const Hero = () => {
   return (
     <div className='hero container'>
