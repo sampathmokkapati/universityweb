fcaae6bc2625cad47f765cbf5962fefe32e4b254
sampathmokkapati <sampath@sampaths-MacBook-Air.local>
2024-10-13 22:47:23 +0530

    running

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
-
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: [
-        /^node:.*/,
-      ]
-    }
-  }
+  plugins: [react()],
 })
